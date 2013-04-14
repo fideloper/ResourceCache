9e4a63a9453efaaee010026d69c57eb25502fd2f
Chris Fidao <fideloper@gmail.com>
2013-04-14 13:17:58 -0700

    Abstracted Request into implementing interface, beginning with a Symfony/Laravel implementation

---
+++ src/Fideloper/ResourceCache/Http/RequestInterface.php
@@ -0,0 +1,15 @@
+<?php namespace Fideloper\ResourceCache\Http;
+
+use Fideloper\ResourceCache\Resource\ResourceInterface;
+
+interface RequestInterface {
+
+    public function wasModified(ResourceInterface $resource);
+
+    public function wasNotModified(ResourceInterface $resource);
+
+    public function getEtags();
+
+    public function getHeader();
+
+}
